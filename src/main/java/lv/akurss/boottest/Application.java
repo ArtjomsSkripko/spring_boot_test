4ec2fa6ee7d9d95133ffd5d65c9674ea6e3fac0d
Test <test@test.com>
2018-02-24 08:56:27 +0200

    Initial commit

---
+++ src/main/java/lv/akurss/boottest/Application.java
@@ -0,0 +1,14 @@
+package lv.akurss.boottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ src/main/java/lv/akurss/boottest/service/TestService.java
@@ -0,0 +1,13 @@
+package lv.akurss.boottest.service;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestService {
+
+    public void test(){
+        System.out.println("I was called");
+    }
+
+}
